c64fec3e28027b773dd153cdeefdbbbd94eefba5
Amir Raj Gurung <amir_grg@hotmail.com>
2019-08-19 16:45:27 +0545

    js up date

--- todos.js
+++ todos.js
@@ -1,5 +1,14 @@
 var btn = document.querySelector("button")
 console.log(btn);
-function submit(e){
+function submit(task,created){
 console.log(e)
+
+}
+document.querySelector("form").onsubmit = function(e){
+    e.preventDefault()
+    console.log(e.srcElement[1],e.srcElement[3])
+    var task = e.srcElement[1].value;
+    var created = e.srcElement[3].value;
+    console.log(task,created);
+    submit(task,created);
 }
